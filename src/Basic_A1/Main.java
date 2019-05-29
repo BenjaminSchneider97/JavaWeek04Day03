a81f2d25b2fe58d8b62bb9f65719750ee2f4ef25
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-29 09:27:49 +0200

    add and finish exercise basic a1

--- src/Basic_A1/Main.java
+++ src/Basic_A1/Main.java
@@ -3,6 +3,14 @@ package Basic_A1;
 public class Main {
 
     public static void main(String[] args) {
+
+        int[] numbers = {5, 2, 6, 8, 1, 4, 9, 3, 0, 7};
+
+        java.util.Arrays.sort(numbers);
+
+        System.out.println(java.util.Arrays.toString(numbers));
+
+        System.out.println("The two biggest numbers are " + numbers[numbers.length-2] + " and " + numbers[numbers.length-1]);
         
     }
 }
